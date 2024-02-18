2483661d01cdfa1af7d56d03e5616641a89554ba
hadaroadams <hadaroadams1234@gmail.com>
2024-02-18 22:57:33 +0000

    sorting error fixed

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -35,15 +35,16 @@ const getAllStaticProduts = async (req, res) => {
       }
     });
   }
-  let result = await Products.find(queryObject);
+  let result = Products.find(queryObject);
+  console.log(result)
   if (sort) {
-    const sortList = sort.slit(",").join(" ");
+    const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
-    result = result.sort("price");
+    result = result.sort({ createdAt: 1 });
   }
   if (fields) {
-    const fieldsList = sort.slit(",").join(" ");
+    const fieldsList = sort.split(",").join(" ");
     result = result.select(fieldsList);
   }
   const page = Number(req.query.page) || 1;
@@ -51,8 +52,8 @@ const getAllStaticProduts = async (req, res) => {
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
-  const products = await result.save();
-  res.send(200).json({ products, nbHits: products.length });
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getallProducts, getAllStaticProduts };
